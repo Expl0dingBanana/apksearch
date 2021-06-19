aede76fc02a861a34139cc8920c1f55b8d38ae6d
Expl0dingBanana <chris.dohmen11@gmail.com>
2021-06-19 09:13:07 -0400

    [refactor](core): coroutine update
    Update the functions within search to be coroutines. This enables non-blocking usage of the package
    
     * Update functions to  be coroutines

--- apksearch/__init__.py
+++ apksearch/__init__.py
@@ -1,3 +1,17 @@
+import logging
+
+import nest_asyncio
+
 from .search import package_search
 
+nest_asyncio.apply()
+
 __all__ = ["package_search"]
+
+
+logger = logging.getLogger(__name__)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+ch.setFormatter(formatter)
+logger.addHandler(ch)
